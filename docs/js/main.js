f79f5289d623adcba7f947d15ee934fc42d98486
AleksandrP23 <30500631+AleksandrP23@users.noreply.github.com>
2021-06-03 18:44:19 +0300

    Add files via upload
    slider

--- docs/js/main.js
+++ docs/js/main.js
@@ -78,6 +78,48 @@ startPlay.forEach(function (item, index) {
 });
 "use strict";
 
+// const anchors = document.querySelectorAll('a.nav__link')
+// for (let anchor of anchors) {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     const blockID = anchor.getAttribute('href')
+//     document.querySelector(blockID).scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     })
+//   })
+// }
+// собираем все якоря; устанавливаем время анимации и количество кадров
+var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    animationTime = 300,
+    framesCount = 20;
+anchors.forEach(function (item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function (e) {
+    // убираем стандартное поведение
+    e.preventDefault(); // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+
+    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset; // запускаем интервал, в котором
+
+    var scroller = setInterval(function () {
+      // считаем на сколько скроллить за 1 такт
+      var scrollBy = coordY / framesCount; // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      } // время интервала равняется частному от времени анимации и к-ва кадров
+
+    }, animationTime / framesCount);
+  });
+});
+"use strict";
+
 var slider = document.querySelector('.swiper-container');
 var mySwiper = new Swiper(slider, {
   slidesPerView: 1,
@@ -97,135 +139,123 @@ var mySwiper = new Swiper(slider, {
     }
   }
 });
-var mySwipers;
-var sliders = document.querySelector('.slider-container');
 
-function mobileSlider() {
-  if (window.innerWidth <= 545 && sliders.dataset.mobile == 'false') {
-    mySwipers = new Swiper(sliders, {
-      slidesPerView: 1.25,
-      spaceBetween: 10,
-      loop: true,
-      slideClass: 'product__item' // pagination: {
-      // 	el: '.swiper-pagination',
-      // 	clickable: true,
-      // },
+if (document.querySelector('.slider-container')) {
+  var mobileSliders = function mobileSliders() {
+    if (window.innerWidth <= 545 && sliders.dataset.mobile == 'false') {
+      mySwipers = new Swiper(sliders, {
+        slidesPerView: 1.25,
+        spaceBetween: 10,
+        loop: true,
+        slideClass: 'product__item' // pagination: {
+        // 	el: '.swiper-pagination',
+        // 	clickable: true,
+        // },
 
-    });
-    sliders.dataset.mobile = 'true';
-  }
-
-  if (window.innerWidth > 545) {
-    sliders.dataset.mobile = 'false';
-
-    if (sliders.classList.contains('swiper-container-initialized')) {
-      mySwipers.destroy();
+      });
+      sliders.dataset.mobile = 'true';
     }
-  }
+
+    if (window.innerWidth > 545) {
+      sliders.dataset.mobile = 'false';
+
+      if (sliders.classList.contains('swiper-container-initialized')) {
+        mySwipers.destroy();
+      }
+    }
+  };
+
+  var mySwipers;
+  var sliders = document.querySelector('.slider-container');
+  mobileSliders();
+  window.addEventListener('resize', function () {
+    mobileSliders();
+  });
 }
 
-mobileSlider();
-window.addEventListener('resize', function () {
-  mobileSlider();
-});
-var mySwipers1;
-var sliders1 = document.querySelector('.slider-container-1');
+if (document.querySelector('.slider-container-1')) {
+  var mobileSliders1 = function mobileSliders1() {
+    if (window.innerWidth <= 545 && sliders1.dataset.mobile == 'false') {
+      mySwipers1 = new Swiper(sliders1, {
+        slidesPerView: 1.25,
+        spaceBetween: 10,
+        loop: true,
+        slideClass: 'product__item' // pagination: {
+        // 	el: '.swiper-pagination',
+        // 	clickable: true,
+        // },
 
-function mobileSliders1() {
-  if (window.innerWidth <= 545 && sliders1.dataset.mobile == 'false') {
-    mySwipers1 = new Swiper(sliders1, {
-      slidesPerView: 1.25,
-      spaceBetween: 10,
-      loop: true,
-      slideClass: 'product__item' // pagination: {
-      // 	el: '.swiper-pagination',
-      // 	clickable: true,
-      // },
-
-    });
-    sliders1.dataset.mobile = 'true';
-  }
-
-  if (window.innerWidth > 545) {
-    sliders1.dataset.mobile = 'false';
-
-    if (sliders1.classList.contains('swiper-container-initialized')) {
-      mySwipers1.destroy();
+      });
+      sliders1.dataset.mobile = 'true';
     }
-  }
-}
 
-mobileSliders1();
-window.addEventListener('resize', function () {
+    if (window.innerWidth > 545) {
+      sliders1.dataset.mobile = 'false';
+
+      if (sliders1.classList.contains('swiper-container-initialized')) {
+        mySwipers1.destroy();
+      }
+    }
+  };
+
+  var mySwipers1;
+  var sliders1 = document.querySelector('.slider-container-1');
   mobileSliders1();
-});
-var mySwipers2;
-var sliders2 = document.querySelector('.slider-container-2');
-
-function mobileSliders2() {
-  if (window.innerWidth <= 545 && sliders2.dataset.mobile == 'false') {
-    mySwipers2 = new Swiper(sliders2, {
-      slidesPerView: 1.25,
-      spaceBetween: 10,
-      loop: true,
-      slideClass: 'product__item' // pagination: {
-      // 	el: '.swiper-pagination',
-      // 	clickable: true,
-      // },
-
-    });
-    sliders2.dataset.mobile = 'true';
-  }
-
-  if (window.innerWidth > 545) {
-    sliders2.dataset.mobile = 'false';
-
-    if (sliders2.classList.contains('swiper-container-initialized')) {
-      mySwipers2.destroy();
-    }
-  }
+  window.addEventListener('resize', function () {
+    mobileSliders1();
+  });
 }
 
-mobileSliders2();
-window.addEventListener('resize', function () {
+if (document.querySelector('.slider-container-2')) {
+  var mobileSliders2 = function mobileSliders2() {
+    if (window.innerWidth <= 545 && sliders2.dataset.mobile == 'false') {
+      mySwipers2 = new Swiper(sliders2, {
+        slidesPerView: 1.25,
+        spaceBetween: 10,
+        loop: true,
+        slideClass: 'product__item' // pagination: {
+        // 	el: '.swiper-pagination',
+        // 	clickable: true,
+        // },
+
+      });
+      sliders2.dataset.mobile = 'true';
+    }
+
+    if (window.innerWidth > 545) {
+      sliders2.dataset.mobile = 'false';
+
+      if (sliders2.classList.contains('swiper-container-initialized')) {
+        mySwipers2.destroy();
+      }
+    }
+  };
+
+  var mySwipers2;
+  var sliders2 = document.querySelector('.slider-container-2');
   mobileSliders2();
-});
-var mySwiperIntro;
-var sliderIntro = document.querySelector('.slider-container-mob');
-
-function mobileSliderIntro() {
-  if (window.innerWidth <= 768 && sliderIntro.dataset.mobile == 'false') {
-    mySwiperIntro = new Swiper(sliderIntro, {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      loop: true,
-      slideClass: 'intro-mob__item' // pagination: {
-      // 	el: '.swiper-pagination',
-      // 	clickable: true,
-      // },
-
-    });
-    sliderIntro.dataset.mobile = 'true';
-  }
-
-  if (window.innerWidth > 768) {
-    sliderIntro.dataset.mobile = 'false';
-
-    if (sliderIntro.classList.contains('swiper-container-initialized')) {
-      mySwiperIntro.destroy();
-    }
-  }
+  window.addEventListener('resize', function () {
+    mobileSliders2();
+  });
 }
 
-mobileSliderIntro();
-window.addEventListener('resize', function () {
-  mobileSliderIntro();
+var sliderI = document.querySelector('.intro-mob');
+var mySwiperI = new Swiper(sliderI, {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination-i',
+    clickable: true
+  }
 });
 "use strict";
 
-videojs(document.querySelector('.video-js'), {
-  language: 'ru'
-});
+if (document.querySelector('.video-js')) {
+  videojs(document.querySelector('.video-js'), {
+    language: 'ru'
+  });
+}
 /**
   * название функции
   *
